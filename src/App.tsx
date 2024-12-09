6c95171f8aab08faa10928459354304e7cfce6b3
MDBBee <bobbyugbebor@gmail.com>
2024-12-09 09:01:42 +0200

    05 ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Component from './starter/04-events';
+import Component from './starter/05-challenge';
 
 function App() {
   return (
     <main>
-      <Component />
+      {/* <Component type="basic" name="susan" /> */}
+      <Component type="advance" name="Banna" email="anna@mail.com" />
+      <Component type="basic" name="Anna" />
     </main>
   );
 }
